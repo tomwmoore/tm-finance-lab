064c77af1f0a37c632960153ff54882caa9fb207
Moore, Thomas Walter <tmoore@gatech.edu>
2025-10-08 22:19:59 -0600

    create function to get prize data and save in azure

--- src/pipelines/get_prices.py
+++ src/pipelines/get_prices.py
@@ -1,12 +1,12 @@
 import sys 
 import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))) # GROSS AND HACKY SO YOU CAN USE src 
 
 import pandas as pd
 import numpy as np
 import yfinance as yf
-from datetime import datetime
-
+from datetime import datetime, timedelta
 
 from src.utils.db_azure import get_analytics_azure_engine
 from src.utils.db_azure import azure_upsert
@@ -14,6 +14,9 @@ from src.utils.db_azure import azure_upsert
 
 def update_prices(industry: str, start_date: str, end_date:str):
 
+    
+    print(f"Debug: Getting price data for industry {industry} and dates: {start_date} to {end_date}...")
+    
     # Get list of tickers from given industry from asset header
     query_symbol = f"""
                     select symbol 
@@ -25,10 +28,34 @@ def update_prices(industry: str, start_date: str, end_date:str):
 
     symbols = pd.read_sql(query_symbol,engine)
 
+    # pull price data
+    data = yf.download(symbols['symbol'].tolist(), interval= '1d', group_by='ticker', start = start_date, end = end_date)
+
+    # Clean up multi-index column names to just be the feature names (close, open etc) & make lowercase
+    data = data.stack(level=0,future_stack=True).rename_axis(['Date', 'Ticker']).reset_index(level=1)
+
+    # remote date as the index
+    data = data.reset_index()
+
+    data.columns = [col.lower() for col in data.columns]
+
+    # add current timestamp for troubleshooting
+    data['updated_at'] = pd.Timestamp.now()
+
+    data = data.rename(columns = {"ticker":"symbol"})
+
+    # Upsert to db
+    print(f"Debug: Upserting price data to stock_prices...")
+
+    azure_upsert(data,engine,'stock_prices')
+
+    print(f"Success: Updating price data complete")
 
 
 
-# for testing
+
+
+# for testing or running individually
 if __name__ == '__main__':
-    print('Testing update_prices.py')
-    update_prices(industry='Oil & Gas E&P', start_date='2025-07-07 13:35:00.000', end_date='2025-07-20 13:35:00.000')
+    print('Running update_prices.py')
+    update_prices(industry='Oil & Gas E&P', start_date='2020-01-01', end_date='2025-10-08')
