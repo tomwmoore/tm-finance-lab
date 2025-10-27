a895541cf0cad0c06712e75ce90e92fd73edd782
Moore, Thomas Walter <tmoore@gatech.edu>
2025-10-26 21:10:28 -0600

    Added function to update WTI prices

--- src/pipelines/get_prices.py
+++ src/pipelines/get_prices.py
@@ -12,51 +12,15 @@ from src.utils.db_azure import get_analytics_azure_engine
 from src.utils.db_azure import azure_upsert
 
 
-
-
 def get_yf_prices(symbols : list, start_date: str, end_date : str, interval : str = '1d'):
 
-    # pull price data
-    data = yf.download(symbols, interval= symbols, group_by='ticker', start = start_date, end = end_date)
-
-    # Clean up multi-index column names to just be the feature names (close, open etc) & make lowercase
-    data = data.stack(level=0,future_stack=True).rename_axis(['Date', 'Ticker']).reset_index(level=1)
-
-    # remote date as the index
-    data = data.reset_index()
-
-    data.columns = [col.lower() for col in data.columns]
-
-    # add current timestamp for troubleshooting
-    data['updated_at'] = pd.Timestamp.now()
-
-    return data
-
-
-
-
-
-
-
-def update_stock_prices(industry: str, start_date: str, end_date:str):
-
-    
-    print(f"Debug: Getting price data for industry {industry} and dates: {start_date} to {end_date}...")
-    
-    # Get list of tickers from given industry from asset header
-    query_symbol = f"""
-                    select symbol 
-                    from asset_header
-                    where industry = '{industry}'
-                    and asset_type = 'stock'
-                    """
-
-    engine = get_analytics_azure_engine()
-
-    symbols = pd.read_sql(query_symbol,engine)
+    '''
+    Returns a pandas dataframe with price data from yfinance for given period and list of symbols. 
+    Data is extracted and column names are standardized. 
+    '''
 
     # pull price data
-    data = yf.download(symbols['symbol'].tolist(), interval= '1d', group_by='ticker', start = start_date, end = end_date)
+    data = yf.download(symbols, interval= interval, group_by='ticker', start = start_date, end = end_date)
 
     # Clean up multi-index column names to just be the feature names (close, open etc) & make lowercase
     data = data.stack(level=0,future_stack=True).rename_axis(['Date', 'Ticker']).reset_index(level=1)
@@ -71,18 +35,60 @@ def update_stock_prices(industry: str, start_date: str, end_date:str):
 
     data = data.rename(columns = {"ticker":"symbol"})
 
+    return data
+
+
+def update_stock_prices(industry: str, start_date: str, end_date:str):
+
+    print(f"Debug: Getting price data for industry {industry} and dates: {start_date} to {end_date}...")
+    
+    # Get list of tickers from given industry from asset header
+    query_symbol = f"""
+                    select symbol 
+                    from asset_header
+                    where industry = '{industry}'
+                    and asset_type = 'stock'
+                    """
+
+    engine = get_analytics_azure_engine()
+
+    symbols = pd.read_sql(query_symbol,engine)
+    symbols = symbols['symbol'].tolist()
+
+    # get price data
+    data = get_yf_prices(symbols=symbols, 
+                         start_date=start_date,
+                         end_date=end_date,
+                         interval='1d')
+
     # Upsert to db
     print(f"Debug: Upserting price data to stock_prices...")
-
     azure_upsert(data,engine,'stock_prices')
-
     print(f"Success: Updating price data complete")
 
 
+
+def update_oil_prices(start_date : str, end_date : str):
+
+    print(f"Debug: Getting oil price data for dates: {start_date} to {end_date}...")
+
+    data = get_yf_prices(symbols=['CL=F'],start_date=start_date, end_date=end_date, interval = '1d', )
+
+    # Upsert to db
+    print(f"Debug: Upserting price data to stock_prices...")
+    engine = get_analytics_azure_engine()
+    azure_upsert(data,engine,'stock_prices')
+    print(f"Success: Updating price data complete")
 
 
 
 # for testing or running individually
 if __name__ == '__main__':
     print('Running update_prices.py')
-    update_stock_prices(industry='Oil & Gas E&P', start_date='2025-10-01', end_date='2025-10-26')
+    start_date = '2020-01-01'
+    end_date ='2025-10-26'
+
+    # update_stock_prices(industry='Oil & Gas E&P', start_date=start_date, end_date=end_date)
+    update_oil_prices(start_date=start_date, end_date=end_date)
+    # test = get_yf_prices(symbols=['CL=F'],start_date=start_date, end_date=end_date)
+    # print(test)
